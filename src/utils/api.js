a5f316693793d886ed75925538b13f574a39145c
Jhoandris B <andybaronp@gmail.com>
2023-08-09 07:42:04 -0500

    utils

--- src/utils/api.js
+++ src/utils/api.js
@@ -84,3 +84,45 @@ export const getRepository = async (id, repository) => {
   const repositoryData = await response.json()
   return repositoryData
 }
+
+
+/**
+ * 
+ * @param {username} username a consultar
+ * @returns  lista de repositorios del usuario
+ */
+export const getUserRepositories = async (id, page = 1) => {
+  try {
+    const response = await getAllDataGet(`${GITHUB_API_BASE_URL}/users/${id}/repos?per_page=15&page=${page}`, {
+      caches: 'no-cache'
+    })
+    const pagination = response.headers.get('link')
+    const lastPageLink = pagination.split(',')
+    const includeNext = lastPageLink.find(link => link.includes('next')).split(';')[0]
+    const indexOfSince = includeNext.indexOf('15&page=')
+    const nextPageSince = includeNext.substring(indexOfSince + 8)
+    const nextPage = nextPageSince.slice(0, -1)
+
+    const includeLast = lastPageLink.find(link => link.includes('last')).split(';')[0]
+    const indexOfLastSince = includeLast.indexOf('15&page=')
+    const lastPageSince = includeLast.substring(indexOfLastSince + 8)
+    const lastPage = lastPageSince.slice(0, -1)
+    // const includeLast = lastPageLink.find(link => link.includes('last')).slice(0, -1)
+    // const indexOfLastSince = includeNext.split('=')
+    // const lastPage = indexOfLastSince[indexOfLastSince.length - 1]
+    const repositories = await response.json()
+
+    return {
+      repositories,
+      nextPage: nextPage,
+      lastPage: lastPage,
+      error: false
+    }
+
+  } catch (error) {
+    return {
+      repositories: [],
+      error: false,
+    }
+  }
+}
---
+++ src/utils/formatDate.js
@@ -0,0 +1,13 @@
+
+
+
+export const formatDate = day => {
+  const newDay = new Date(day)
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit'
+  }
+
+  return newDay.toLocaleDateString('es-ES', options)
+}
